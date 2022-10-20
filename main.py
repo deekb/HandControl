9e71c671c7210d997f5f8d94ae618c76efb7651b
deekb <derek.m.baier@gmail.com>
2022-10-20 10:12:06 -0400

    Updated to work with both left and right clicking and some optimization of the clamping function

--- main.py
+++ main.py
@@ -2,29 +2,36 @@ import cv2
 import mediapipe as mp
 import pyautogui
 import tkinter
+import os
+import math
+import time
+
 root = tkinter.Tk()
 
 pyautogui.PAUSE = 0
 pyautogui.FAILSAFE = False
 
-BUFFER = 0.9
+BUFFER = 0.75
 SHOW_IMAGE = True
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-screen_width, screen_height = (root.winfo_screenwidth(),root.winfo_screenheight()) #(1920, 1080)
+smoothedValues = []
+
+screen_width, screen_height = (root.winfo_screenwidth(), root.winfo_screenheight())  # (1920, 1080)
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
         model_complexity=0,
-        min_detection_confidence=0.5,
+        max_num_hands=1,
+        min_detection_confidence=0.75,
         min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
         success, image = cap.read()
         image = cv2.flip(image, 1)
         if not success:
-            print("Ignoring empty camera frame.")
+            print("[WARN]: Got blank or invalid frame from camera")
             continue
 
         # To improve performance, optionally mark the image as not writeable to
@@ -44,24 +51,49 @@ with mp_hands.Hands(
                     mp_hands.HAND_CONNECTIONS,
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style())
+                
+                middleX = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x
+                middleY = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y
+                
+                indexX = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x
+                indexY = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
+                
+                thumbX = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x
+                thumbY = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y
+                
+                
+                
+                normalizedX = (indexX + thumbX) / 2
+                normalizedY = (indexY + thumbY) / 2
+                
+                smoothedValues.append((normalizedX, normalizedY))
+                if len(smoothedValues) > 10:
+                    smoothedValues = smoothedValues[-10:]
+                
+                smoothedX = sum([pair[0] for pair in smoothedValues]) / len(smoothedValues)
+                smoothedY = sum([pair[1] for pair in smoothedValues]) / len(smoothedValues)
+                                
+                if math.sqrt((abs(indexX - thumbX) + abs(indexY - thumbY))) < 0.4:
+                    if math.sqrt((abs(middleX - indexX) + abs(middleY - indexY))) < 0.2 and middleY < indexY + 10:
+                        pyautogui.mouseDown(button='right')
+                    else:
+                        pyautogui.mouseDown(button='left')
+                elif not(math.sqrt((abs(indexX - thumbX) + abs(indexY - thumbY))) < 0.4):
+                    pyautogui.mouseUp(button='left')
+                    pyautogui.mouseUp(button='right')
+                
+                
+                indexFingerTipX = (((smoothedX - 0.5) / BUFFER) + 0.5) * screen_width
+                indexFingerTipY = (((smoothedY - 0.5) / BUFFER) + 0.5) * screen_height
 
-                indexFingerTipX = (((hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x - 0.5) / BUFFER) + 0.5) * screen_width
-                indexFingerTipY = (((hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y - 0.5) / BUFFER) + 0.5) * screen_height
-                print(f"\r({indexFingerTipX}, {indexFingerTipY})", end="")
-                if indexFingerTipX > screen_width:
-                    indexFingerTipX = screen_width
-                elif indexFingerTipX < 0:
-                    indexFingerTipX = 0
-                if indexFingerTipY > screen_height:
-                    indexFingerTipY = screen_height
-                elif indexFingerTipY < 0:
-                    indexFingerTipY = 0
-
+                print(f"\r({indexFingerTipX}, {indexFingerTipY})", end=(" " * 100))
+                indexFingerTipX = sorted((0, indexFingerTipX, screen_width))[1]  # clamp the coordinates to the screen
+                indexFingerTipY = sorted((0, indexFingerTipY, screen_height))[1]
 
                 pyautogui.moveTo(int(indexFingerTipX), int(indexFingerTipY))
-
 
         if SHOW_IMAGE: cv2.imshow("Image", image)
         if cv2.waitKey(1) & 0xFF == 27:
             break
 cap.release()
+os._exit(0)
